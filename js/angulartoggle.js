6f16d4eb0ad81505e4d908eb20cc4d113f86b0e5
Desarae <desaraev@users.noreply.github.com>
2016-06-19 10:53:54 -0500

    create angulartoggle.js

---
+++ js/angulartoggle.js
@@ -0,0 +1,81 @@
+angular.module('ionicApp', ['ngStorage','webstorage'])
+
+.controller('MainCtrl', function($scope, $localStorage) {
+  
+  // Primer paso
+  $scope.strings = ['Main Theme', 'Night Theme', 'Sunny Theme', 'Magic Theme'];
+  
+  
+  $scope.cargarTemplates = function () {
+    if ($scope.template == 0) {
+      alert("hola")
+    }
+  }
+  
+})
+  
+
+function componentPrepare() {
+  $(".select.switch label").each(function(index) {
+    var label = $(this);
+    var id = label.attr("for");
+    var select = label.find("select");
+    var $body = $('body');
+    var $bg = $('#bg');
+
+    var MAX_NUM = 4;
+    var selected = 0;
+    var values = [];
+
+    //read options
+    select.find("option").each(function(index) {
+      if (index < MAX_NUM) {
+        values[index] = {
+          value: $(this).val(),
+          label: $(this).html()
+        };
+        if ($(this).is(":selected") || select.val() == $(this).val())
+          selected = index;
+      }
+    });
+
+    //setting sizes
+    label.find(".switch-toggle").css("width", 7.5 + values.length * 15.5);
+
+    //selecting default
+    label.find(".switch-toggle").val(values[selected].value);
+    label.find(".display").html(values[selected].label);
+    label.addClass("selected-" + selected);
+      $body.addClass("bg-" + selected);
+    
+    //change on click
+    label.click(function(event) {
+      selected = (selected + 1) % values.length;
+      select.val(values[selected].value).change();
+      event.preventDefault();
+    });
+
+    // select change event
+    select.change(function(event) {
+      var selected = 0;
+      for (var i = 0; i < values.length; i++) {
+        label.removeClass("selected-" + i);
+        $body.removeClass("bg-" + i);
+        if ($(this).val() == values[i].value)
+          selected = i;
+      }
+
+      label.find(".display").html(values[selected].label);
+      label.addClass("selected-" + selected);
+      $body.addClass("bg-" + selected);
+      event.preventDefault();
+    });
+
+  });
+}
+
+$(document).ready(function() {
+
+  componentPrepare();
+
+});
