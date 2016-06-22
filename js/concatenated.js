2ec9dae2b99545f59bad51678539e5e19a5fbb13
Desarae <desaraev@users.noreply.github.com>
2016-06-21 20:16:20 -0500

    concatenated js and minified

---
+++ js/concatenated.js
@@ -0,0 +1,7 @@
+//@prepros-prepend bootstrap.min.js
+//@prepros-prepend docs.min.js
+//@prepros-prepend angular.min.js
+//@prepros-prepend ionic.js
+//@prepros-prepend ngStorage.js
+//@prepros-prepend webstorage.min.js
+//@prepros-prepend angulartoggle.js
