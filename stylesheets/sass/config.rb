d48b01004d34fa6bfa6671db94a1ad10fa170711
Desarae <desaraev@users.noreply.github.com>
2016-06-21 19:53:56 -0500

    configuring css

---
+++ stylesheets/sass/config.rb
@@ -0,0 +1,22 @@
+# Set this to the root of your project when deployed:
+# http_path = "https://rawgit.com/Desaraevdotcom/portfolio/blogger/"
+
+# Set the images directory relative to your http_path or change
+# the location of the images themselves using http_images_path:
+http_images_dir = "Desaraevdotcom/portfolio/blogger/stylesheets/sass/images/"
+
+# Production Assets URL
+http_images_path = "Desaraevdotcom/portfolio/blogger/stylesheets/sass/images/"
+
+css_dir = "min"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
+
+output_style = :compressed
+environment = :development
+
+relative_assets = false
+
+line_comments = false
+color_output = false
